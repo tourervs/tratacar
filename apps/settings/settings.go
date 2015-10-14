c107c883bf9851e483937900a898a1d85dfcc28a
Marat <tourervs@gmail.com>
2015-10-14 14:39:51 +0300

    14102015-1439q

--- apps/settings/settings.go
+++ apps/settings/settings.go
@@ -1,4 +1,12 @@
 package settings
 
-var STATIC_DIR = "/tratacar/apps/static/"
+var STATIC_DIR      = "/tratacar/apps/static/"
+var STATIC_URL      = "/static/"
+var LISTEN_PORT     = "80"
+var LISTEN_ADDRESS  = "0.0.0.0"
 
+type Settings struct {
+
+
+
+}
