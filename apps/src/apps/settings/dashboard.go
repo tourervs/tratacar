128f2a6d20ea4c8f178364ab3f256376dd655b34
Marat <tourervs@gmail.com>
2015-10-14 20:57:11 +0300

    14102015-2056q

--- apps/src/apps/settings/dashboard.go
+++ apps/src/apps/settings/dashboard.go
@@ -11,7 +11,7 @@ type Dashboard struct {
 
 }
 
-func (dashboard *Dashboard ) AddNew () {
+func (dashboard *Dashboard ) Add () {
 
 
 
---
+++ core/src/core/web/page/compute.go
@@ -0,0 +1,27 @@
+package page
+
+type Page struct {
+
+
+}
+
+type Content struct {
+
+
+}
+
+type LeftSidePanel struct {
+
+
+}
+
+type TopPanel struct {
+
+
+
+}
+
+type SettingsPanel struct {
+
+
+}
