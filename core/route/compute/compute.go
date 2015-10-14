c5dfcdcfe8cf7c2595fb102706686d601904d7bc
root <root@zombie.(none)>
2015-10-14 20:18:09 +0300

    13102015-2116q

---
+++ core/route/compute/compute.go
@@ -0,0 +1,5 @@
+package compute
+// search nerby
+
+
+
--- core/route/compute/route.go
+++ core/route/compute/route.go
@@ -1,21 +1,26 @@
+package compute
+
 type Route struct {
 
-    Price       int
-    CheckPoints []CheckPoint
-
+    Price        int
+    Value        string
+    CheckPoints  []CheckPoint
+    TimeTables   []TimeTable
 
 }
 
 type CheckPoint struct {
 
-    Lat     float32
-    Lon     float32
-    Country string
-    Region  string
-    City    string
-    Area    string
-    Street  string
-    House   string
+    Lat      float32
+    Lon      float32
+    Country  string
+    Region   string
+    City     string
+    Area     string
+    Street   string
+    House    string
+    Source   bool
+    Dest     bool
 
 }
 
